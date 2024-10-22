c1c4d3f1a7413c923b7ced1004d83c36fb43fbc8
drielsz <adrielllxavi@outlook.com>
2024-10-21 22:31:26 -0300

    git pool errors

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -10,36 +10,23 @@ function handleDisconnect() {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        keepAliveInitialDelay: 10000,
-        enableKeepAlive: true,
         waitForConnections: true,
-        connectTimeout: 60000, // Timeout de 60 segundos
-        acquireTimeout: 60000, // Tempo limite para adquirir uma conexão
-        queueLimit: 0
+        connectionLimit: 10, 
+        queueLimit: 0,
+        connectTimeout: 100000,
     });
 
     conexao.getConnection((err, connection) => {
         if (err) {
             console.error(`[${new Date().toISOString()}] Erro ao conectar ao MySQL:`, err);
-            setTimeout(handleDisconnect, 2000); // Continua tentando reconectar a cada 2 segundos
+            setTimeout(handleDisconnect, 2000); // Continua tentando reconectar
         } else {
             console.log(`[${new Date().toISOString()}] Conectado ao MySQL`);
-            if (connection) connection.release(); // Libera a conexão de volta ao pool
-        }
-    });
-
-    conexao.on('error', err => {
-        console.error(`[${new Date().toISOString()}] Erro na conexão MySQL:`, err);
-    
-        if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET' || err.code === 'ETIMEDOUT') {
-            console.log('Tentando reconectar após erro de perda de conexão...');
-            handleDisconnect(); // Reconectar automaticamente em caso de perda de conexão
-        } else {
-            throw err;
+            connection.release(); // Libera a conexão de volta ao pool
         }
     });
 }
 
 handleDisconnect();
 
-module.exports = conexao.promise();
+module.exports = conexao;
--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -1,27 +1,28 @@
 class Tabelas {
-    async init(conexao) {
+    init(conexao) {
         this.conexao = conexao;
-        await this.createTableAtendimento(); // Use await para garantir que a tabela seja criada antes de continuar
+        this.createTableAtendimento();
     }
 
-    async createTableAtendimento() {
+    createTableAtendimento() {
         const sql = `
             CREATE TABLE IF NOT EXISTS cliente_atendimento (
-            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            DATA DATE, 
-            horario_atendimento TIME,
-            servico VARCHAR(100),
-            cliente VARCHAR(100),
-            STATUS ENUM ("ativo", "realizado", "cancelado") DEFAULT "ativo"
-        );
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                DATA DATE, 
+                horario_atendimento TIME,
+                servico VARCHAR(100),
+                cliente VARCHAR(100),
+                STATUS ENUM ("ativo", "realizado", "cancelado") DEFAULT "ativo"
+            );
         `;
 
-        try {
-            await this.conexao.query(sql); // Usando await com a query
-            console.log(200); // Sucesso
-        } catch (error) {
-            console.log(`error ${error.message}`); // Captura erros
-        }
+        this.conexao.query(sql, (error) => {
+            if (error) {
+                console.log(`Erro ao criar tabela: ${error.message}`);
+            } else {
+                console.log("Tabela criada ou já existente");
+            }
+        });
     }
 }
 
